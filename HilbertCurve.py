07b32f9fb73ca6f704d72a7933bab44fb5bcc3c8
Jeongseop Lee <wanderkind@semyung.ac.kr>
2021-12-01 23:10:27 +0900

    Update HilbertCurve.py

--- HilbertCurve.py
+++ HilbertCurve.py
@@ -11,32 +11,26 @@ while True:
         point = float(input('Float between 0 and 1 : '))
         continue
 
-def PHC(n, point):
+pointround = round((4**n - 1)*point)
 
-    pointround = round((4**n - 1)*point)
+L = []
+for i in range(n):
+    a = pointround//(4**(n - 1 - i))
+    pointround = pointround%(4**(n - 1 - i))
+    L.append([a, pointround])
 
-    L = []
-    for i in range(n):
-        a = pointround//(4**(n - 1 - i))
-        pointround = pointround%(4**(n - 1 - i))
-        L.append([a, pointround])
+N = [[0, 0]]
+for i in range(n):
+    if L[n - 1 - i][0] == 0:
+        N.append([-1/4 + N[i][1]/2, -1/4 + N[i][0]/2])
+    
+    elif L[n - 1 - i][0] == 1:
+        N.append([-1/4 + N[i][0]/2, 1/4 + N[i][1]/2])
+    
+    elif L[n - 1 - i][0] == 2:
+        N.append([1/4 + N[i][0]/2, 1/4 + N[i][1]/2])
+    
+    else:
+        N.append([1/4 - N[i][1]/2, -1/4 - N[i][0]/2])
 
-    N = [[0, 0]]
-
-    for i in range(n):
-        if L[n - 1 - i][0] == 0:
-            N.append([-1/4 + N[i][1]/2, -1/4 + N[i][0]/2])
-        
-        elif L[n - 1 - i][0] == 1:
-            N.append([-1/4 + N[i][0]/2, 1/4 + N[i][1]/2])
-        
-        elif L[n - 1 - i][0] == 2:
-            N.append([1/4 + N[i][0]/2, 1/4 + N[i][1]/2])
-        
-        else:
-            N.append([1/4 - N[i][1]/2, -1/4 - N[i][0]/2])
-
-    return [round(1/2 + N[n][0], 8), round(1/2 + N[n][1], 8)]
-
-H = PHC(n, point)
-print(H)
+print([round(1/2 + N[n][0], 8), round(1/2 + N[n][1], 8)])
